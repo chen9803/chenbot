40c4324fa85565bdb3428c6fb5644a565b0d1cc6
edwardshturman <>
2020-08-19 22:01:19 -0700

    Create index.js
    Just the basics to get going

---
+++ index.js
@@ -0,0 +1,26 @@
+// Dependencies
+const Discord = require('discord.js');
+
+// Launch
+const client = new Discord.Client();
+
+// Prefix
+const prefix = 'c.';
+
+// Create collection of commands
+client.commands = new Discord.Collection();
+
+// Announce on launch
+client.once('ready', () => {
+    console.log('d.srupt is online!');
+    client.user.setActivity('A COMP', { type: 'STREAMING' }).catch(console.error);
+});
+
+// Check for correct filetype (JavaScript) and require command files when running given command
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+// THE FUN BEGINS
