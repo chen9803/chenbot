914274f8b2d8e8ff68fe8bccfda758b15c7edd1e
edwardshturman <>
2020-08-19 23:12:57 -0700

    a little cursed ngl
    A fun update to keep things "interesting"

--- commands/ping.js
+++ commands/ping.js
@@ -5,10 +5,10 @@ module.exports = {
         const Discord = require('discord.js');
         const pingEmbed = new Discord.MessageEmbed()
             .setColor('#ffffff')
-            .setTitle('THE PING COMMAND')
+            .setTitle('Welcome to the ping command!')
             //.setURL('https://community.dcraft.net/support')
-            .setAuthor(message.channel.lastMessage.member.displayName, message.channel.lastMessage.author.avatarURL())
-            .setDescription('Here is the ping command!')
+            .setAuthor(`Pinged by: ${message.channel.lastMessage.member.displayName}`, message.channel.lastMessage.author.avatarURL())
+            .setDescription('Incoming ping! Ready?')
             //.setThumbnail('https://dcraft.net/wp-content/uploads/2020/06/dtechlogo-transparent.png')
             /*.addFields(
                 {name: 'A field!', value: 'A value!'},
@@ -16,10 +16,10 @@ module.exports = {
                 {name: 'Inline field!', value: 'Inline value!', inline: true},
                 {name: 'Inline field!', value: 'Inline value!', inline: true},
             )*/
-            .addField('Ping', ':ping_pong: Pong!')
+            .addField('Ping!', ':ping_pong: Pong!')
             .setImage('https://media.istockphoto.com/photos/active-senior-playing-pingpong-picture-id516718348')
             //.setTimestamp()
-            .setFooter('This was the ping command!', 'https://www.pngmart.com/files/2/Pokeball-PNG-Photos.png');
+            .setFooter('This was the ping command! Good job!', 'https://www.pngmart.com/files/2/Pokeball-PNG-Photos.png');
         message.channel.send(pingEmbed);
     }
 };
