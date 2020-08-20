4bb4585899da5429c506e7c5bce1c6e88cd59dc8
edwardshturman <>
2020-08-19 22:57:08 -0700

    Update ping.js
    Fixed image locations

--- commands/ping.js
+++ commands/ping.js
@@ -7,7 +7,7 @@ module.exports = {
             .setColor('#000000')
             .setTitle('THE PING COMMAND')
             //.setURL('https://community.dcraft.net/support')
-            .setAuthor(message.channel.lastMessage.member, message.channel.lastMessage.author.avatar)
+            .setAuthor(message.channel.lastMessage.member, message.channel.lastMessage.author.avatarURL())
             .setDescription('Here is the ping command!')
             //.setThumbnail('https://dcraft.net/wp-content/uploads/2020/06/dtechlogo-transparent.png')
             /*.addFields(
@@ -17,7 +17,7 @@ module.exports = {
                 {name: 'Inline field!', value: 'Inline value!', inline: true},
             )*/
             .addField('Ping', ':ping_pong: Pong!')
-            .setImage('../assets/pong.jpg')
+            .setImage('https://media.istockphoto.com/photos/active-senior-playing-pingpong-picture-id516718348')
             //.setTimestamp()
             .setFooter('This was the ping command!', 'https://www.pngmart.com/files/2/Pokeball-PNG-Photos.png');
         message.channel.send(pingEmbed);
