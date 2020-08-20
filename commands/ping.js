937b225e0911c93d6395b284c4233dc4b7fc5da2
edwardshturman <>
2020-08-19 23:02:59 -0700

    Update status and ping
    - Added displayName on ping
    - Changed activity type to PLAYING

--- commands/ping.js
+++ commands/ping.js
@@ -4,10 +4,10 @@ module.exports = {
     execute(message, args) {
         const Discord = require('discord.js');
         const pingEmbed = new Discord.MessageEmbed()
-            .setColor('#000000')
+            .setColor('#ffffff')
             .setTitle('THE PING COMMAND')
             //.setURL('https://community.dcraft.net/support')
-            .setAuthor(message.channel.lastMessage.member, message.channel.lastMessage.author.avatarURL())
+            .setAuthor(message.channel.lastMessage.member.displayName, message.channel.lastMessage.author.avatarURL())
             .setDescription('Here is the ping command!')
             //.setThumbnail('https://dcraft.net/wp-content/uploads/2020/06/dtechlogo-transparent.png')
             /*.addFields(
